31f57ac2d5e7fc2055999252d18f010309814420
Ivaylo <ivailo.atanasov93@gmail.com>
2021-04-14 17:01:13 +0100

    Add User Functionality

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from users.views import login_view, sign_up_view
+from users.views import login_view, sign_up_view, logout_view
 
 urlpatterns = [
     path('', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
     path('sign_up/', sign_up_view, name='sign_up'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,13 +1,42 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import CreateUserForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+from .models import Customer
+
 
 def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            messages.info(request, 'Username OR Password is incorrect')
     context = {}
     return render(request, 'login.html', context)
 
 
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+
 def sign_up_view(request):
-    context = {}
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            name = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            Customer.objects.create(user=user, name=name, email=email)
+            messages.success(request, f'Account was created for {form.cleaned_data.get("username")}')
+            return redirect('login')
+    context = {'form': form}
     return render(request, 'sign_up.html', context)
